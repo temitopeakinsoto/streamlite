9a9e7971f20730059b690b7a64b13858cc3f0555
temitopeakinsoto <takinsoto@gmail.com>
2023-09-16 14:11:10 +0100

    Add webstream image in questionnaire:
    In this commit, the image frame captured during testingis presented
    to the user to validate the accuracy of the emotion being captured

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import json
 import uuid
+import base64
+import csv
+import numpy as np
+
+
 logo = './images/logo.png'
 
 # Create a sidebar with navigation links
@@ -27,6 +32,49 @@ unique_id = str(uuid.uuid4())
 if 'session_state' not in st.session_state:
     st.session_state.session_state = {}
     st.session_state.session_state['unique_id'] = str(uuid.uuid4())
+
+def image_to_base64(image):
+    _, buffer = cv2.imencode('.jpg', image)
+    base64_frame = base64.b64encode(buffer).decode('utf-8')
+    return base64_frame
+
+
+
+def getData():
+    # Specify the CSV file name
+    csv_file = './database/emotion_data.csv'
+    # Initialize variables to store values from row 10
+    emotion = None
+    frame = None
+    # Open the CSV file and read its contents
+    with open(csv_file, mode='r', newline='') as file:
+        csv_reader = csv.reader(file)
+        
+        # Skip the first 9 rows (0-indexed) to reach row 10
+        for _ in range(9):
+            next(csv_reader)
+        try:
+            row = next(csv_reader)
+            # Check if row has at least 3 columns
+            if len(row) >= 3:
+                emotion = row[1]  # Value from col2
+                frame = row[2]  # Value from col3
+            else:
+                print("Row 10 does not have enough columns.")
+        except StopIteration:
+            print("Reached the end of the file before reaching row 10.")
+    # Check if values were found
+    if emotion is not None and frame is not None:
+        binary_image = base64.b64decode(frame)
+
+        # Convert the binary image data to a NumPy array
+        image_array = np.frombuffer(binary_image, np.uint8)
+
+        # Decode the image using OpenCV
+        decoded_image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        return([emotion, decoded_image])
+
+
 
 def consent():
     global consent_data_list
@@ -255,32 +303,34 @@ def questionnaire():
     """)
 
     # Age
-    age = st.selectbox("Please tell us your age:", ["18-24", "25-34", "35-44", "45-54", "55 and above"])
-
+    age = st.selectbox("**Please tell us your age:**", ["18-24", "25-34", "35-44", "45-54", "55 and above"])
+    
     # Gender
-    gender = st.selectbox("Please tell us your gender:", ["Male", "Female", "Non-binary/Other", "Prefer not to say"])
+    gender = st.selectbox("**Please tell us your gender:**", ["Male", "Female", "Non-binary/Other", "Prefer not to say"])
 
     # Ethnicity/Race
-    ethnicity = st.selectbox("What is your Ethnicity/Race:", ["English", "Welsh", "Scottish", "Northern Irish" , "British Irish","Indian", "Indian British", "Chinese", "Chinese British", "Any other Asian", "Caribbean","African", "Black British", "Any other Black", "Asian", "Others"])
+    ethnicity = st.selectbox("**What is your Ethnicity/Race:**", ["English", "Welsh", "Scottish", "Northern Irish" , "British Irish","Indian", "Indian British", "Chinese", "Chinese British", "Any other Asian", "Caribbean","African", "Black British", "Any other Black", "Asian", "Others"])
 
     # Frequency of e-learning activities
-    frequency = st.selectbox("How often do you engage in e-learning activities via online platforms?", ["Never", "Daily", "Weekly", "Monthly", "Rarely", "Never"])
-    learningPreference = st.selectbox("What is your preferred model for learning", ["Face-to-Face Classroom settings", "Online (E-Learning)", "Hybrid Model"])
+    frequency = st.selectbox("**How often do you engage in e-learning activities via online platforms?**", ["Never", "Daily", "Weekly", "Monthly", "Rarely", "Never"])
+    learningPreference = st.selectbox("**What is your preferred model for learning**", ["Face-to-Face Classroom settings", "Online (E-Learning)", "Hybrid Model"])
 
     # Highest level of education
-    education = st.selectbox("Tell us your highest level of education:", ["High School or Below", "Some College/Associate Degree", "Bachelor's Degree", "Master's Degree or Higher"])
-
+    education = st.selectbox("**Tell us your highest level of education:**", ["High School or Below", "Some College/Associate Degree", "Bachelor's Degree", "Master's Degree or Higher"])
+    st.image(getData()[1])
+    agree = st.radio("**According to our app, your emotion in the image above is: **"+getData()[0] + " Do you agree", ["YES", "NO"])
+    
     # Ratings and feedback
-    overall_experience = st.slider("On a scale of 1 - 10 (10 Being the highest), how would you rate your overall experience with using the emotion recognition app?", 1, 10)
-    comfort_level = st.slider("On a scale of 1 - 10 (10 Being the highest), How comfortable were you using the emotion recognition system during the session?", 1, 10)
-    accuracy = st.slider("On a scale of 1-10 (10 Being the highest), How accurately did the system detect your emotional state/facial expressions?", 1, 10)
-    recognized_emotions = st.text_input("Which emotions/facial expressions do you feel were accurately recognized by the system? (Happy, Sad, Angry, Confused, etc.)")
-    misinterpreted_emotions = st.text_input("Were there any emotions/facial expressions that the system misinterpreted? If yes, please specify.")
-    technical_issues = st.radio("Were there any technical issues or challenges while using the emotion recognition app? (YES/NO)", ["YES", "NO"])
+    overall_experience = st.slider("**On a scale of 1 - 10 (10 Being the highest), how would you rate your overall experience with using the emotion recognition app?**", 1, 10)
+    comfort_level = st.slider("**On a scale of 1 - 10 (10 Being the highest), How comfortable were you using the emotion recognition system during the session?**", 1, 10)
+    accuracy = st.slider("**On a scale of 1-10 (10 Being the highest), How accurately did the system detect your emotional state/facial expressions?**", 1, 10)
+    recognized_emotions = st.text_input("**Which emotions/facial expressions do you feel were accurately recognized by the system? (Happy, Sad, Angry, Confused, etc.)**")
+    misinterpreted_emotions = st.text_input("**Were there any emotions/facial expressions that the system misinterpreted? If yes, please specify.**")
+    technical_issues = st.radio("**Were there any technical issues or challenges while using the emotion recognition app? (YES/NO)**", ["YES", "NO"])
     if technical_issues == "YES":
-        issue_details = st.text_input("If yes, please specify.")
+        issue_details = st.text_input("**If yes, please specify.")
 
-    feedback_timeliness = st.radio("Did the system provide timely and relevant feedback to your emotional state based on your facial expression during the session? (YES/NO)", ["YES", "NO"])
+    feedback_timeliness = st.radio("**Did the system provide timely and relevant feedback to your emotional state based on your facial expression during the session? (YES/NO)**", ["YES", "NO"])
     date = st.date_input("Date:")
     # Submit button
     if st.button("Submit"):
@@ -334,7 +384,7 @@ def questionnaire():
         
         st.success("Thank you for completing the questionnaire. Your feedback is invaluable for improving our emotion recognition app for e-learning purposes.")
         st.success("You have now completed this survey and may close the browser!")
-
+        getData()
 
 def apppage():
 
@@ -414,7 +464,7 @@ def apppage():
 
         emotion_timestamps.append(timestamp)
         emotion_values.append(emotion)
-        data = {'Timestamp': emotion_timestamps, 'Emotion': emotion_values, 'ID': st.session_state.session_state['unique_id']}
+        data = {'Timestamp': emotion_timestamps, 'Emotion': emotion_values, 'frame': image_to_base64(resized_frame), 'ID': st.session_state.session_state['unique_id']}
         
         
         df = pd.DataFrame(data)
